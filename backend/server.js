587e4a473fa6294888ff366daa2be6b72ab4d2d1
Mark Anthony Vivar <mvivar@cloudpanda.ph>
2022-08-15 15:45:34 +0800

    made some changes

--- backend/server.js
+++ backend/server.js
@@ -28,9 +28,9 @@ app.use("/api/refunds", require("./routes/refundRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(__dirname, "../", "frontend", "build", "index.html")
-  );
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.status(200).json({ message: "Welcome to the Helpdesk App" });
